0f8762a4445a550753e7e0f4484d02fe053ff9e1
Ben Kogan <benkogan9@gmail.com>
2024-07-20 23:39:36 -0700

    parens

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -8,12 +8,20 @@ import (
 func Tokenize(file io.Reader) []Token {
 	f := bufio.NewReader(file)
 	tokens := make([]Token, 0)
+	var token string
 	for {
-		_, err := f.ReadString(' ')
+		char, _, err := f.ReadRune()
 		if err == io.EOF {
 			tokens = append(tokens, Token{Type: TokenTypeEOF})
 			break
 		}
+		token += string(char)
+		tokenType := typeFromString(token)
+		if tokenType == TokenTypeUnknown {
+			continue
+		}
+		tokens = append(tokens, Token{Type: tokenType, Lexeme: token})
+		token = ""
 	}
 	return tokens
 }
--- internal/lexer/token.go
+++ internal/lexer/token.go
@@ -1,10 +1,39 @@
 package lexer
 
+import "fmt"
+
 type TokenType int
 
 const (
 	TokenTypeEOF TokenType = iota
+	TokenTypeLeftParen
+	TokenTypeRightParen
+	TokenTypeUnknown
 )
+
+func typeFromString(s string) TokenType {
+	switch s {
+	case "(":
+		return TokenTypeLeftParen
+	case ")":
+		return TokenTypeRightParen
+	default:
+		return TokenTypeUnknown
+	}
+}
+
+func (t TokenType) String() string {
+	switch t {
+	case TokenTypeEOF:
+		return "EOF"
+	case TokenTypeLeftParen:
+		return "LEFT_PAREN"
+	case TokenTypeRightParen:
+		return "RIGHT_PAREN"
+	default:
+		panic("Unknown token type")
+	}
+}
 
 type Token struct {
 	Type    TokenType
@@ -17,6 +46,10 @@ func (t *Token) String() string {
 	case TokenTypeEOF:
 		return "EOF  null"
 	default:
-		panic("Unknown token type")
+		literal := t.Literal
+		if literal == "" {
+			literal = "null"
+		}
+		return fmt.Sprintf("%s %s %s", t.Type.String(), t.Lexeme, literal)
 	}
 }
