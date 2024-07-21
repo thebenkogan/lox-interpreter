a3b2d4741adf95a0199f213a95302a01ed60ee69
Ben Kogan <benkogan9@gmail.com>
2024-07-21 13:31:29 -0700

    fix nil

--- internal/parser/printer.go
+++ internal/parser/printer.go
@@ -5,6 +5,9 @@ import "fmt"
 func (e *Expression) String() string {
 	switch e.Type {
 	case ExpressionTypeLiteral:
+		if e.Literal == nil {
+			return "nil"
+		}
 		return fmt.Sprintf("%v", e.Literal)
 	default:
 		return "TODO"
