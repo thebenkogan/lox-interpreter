4f437afaf45404242e1b9aa65b424979cb4f3053
Ben Kogan <benkogan9@gmail.com>
2024-07-21 17:21:33 -0700

    finish evaluate tests

--- internal/evaluator/evaluator_test.go
+++ internal/evaluator/evaluator_test.go
@@ -20,6 +20,131 @@ func TestEvaluator(t *testing.T) {
 			program:  "123",
 			expected: float64(123),
 		},
+		{
+			name:     "parentheses",
+			program:  "(123)",
+			expected: float64(123),
+		},
+		{
+			name:     "unary not",
+			program:  "!true",
+			expected: false,
+		},
+		{
+			name:     "unary not convert to bool",
+			program:  "!nil",
+			expected: true,
+		},
+		{
+			name:        "unary minus wrong type",
+			program:     "-\"hello\"",
+			expectError: true,
+		},
+		{
+			name:     "unary minus",
+			program:  "-3",
+			expected: float64(-3),
+		},
+		{
+			name:     "multiply",
+			program:  "2 * 3",
+			expected: float64(6),
+		},
+		{
+			name:        "multiple wrong types",
+			program:     "2 * true",
+			expectError: true,
+		},
+		{
+			name:     "divide",
+			program:  "2 / 3",
+			expected: float64(2) / float64(3),
+		},
+		{
+			name:        "divide wrong types",
+			program:     "2 / true",
+			expectError: true,
+		},
+		{
+			name:        "divide by zero",
+			program:     "2 / 0",
+			expectError: true,
+		},
+		{
+			name:     "add",
+			program:  "3 + 5",
+			expected: float64(8),
+		},
+		{
+			name:     "add strings",
+			program:  "\"hello\" + \" world\"",
+			expected: "hello world",
+		},
+		{
+			name:        "add wrong types",
+			program:     "3 + true",
+			expectError: true,
+		},
+		{
+			name:     "subtract",
+			program:  "3 - 5",
+			expected: float64(-2),
+		},
+		{
+			name:        "subtract wrong types",
+			program:     "3 - true",
+			expectError: true,
+		},
+		{
+			name:     "greater",
+			program:  "5 > 3",
+			expected: true,
+		},
+		{
+			name:        "greater wrong types",
+			program:     "5 > true",
+			expectError: true,
+		},
+		{
+			name:     "greater equal",
+			program:  "5 >= 5",
+			expected: true,
+		},
+		{
+			name:        "greater equal wrong types",
+			program:     "5 >= true",
+			expectError: true,
+		},
+		{
+			name:     "less",
+			program:  "5 < 3",
+			expected: false,
+		},
+		{
+			name:        "less wrong types",
+			program:     "5 < true",
+			expectError: true,
+		},
+		{
+			name:     "less equal",
+			program:  "5 <= 5",
+			expected: true,
+		},
+		{
+			name:        "less equal wrong types",
+			program:     "5 <= true",
+			expectError: true,
+		},
+		{
+			name:     "equal",
+			program:  "5 == 5",
+			expected: true,
+		},
+		{
+			name:     "not equal",
+			program:  "5 != true",
+			expected: true,
+		},
 	}
 
 	for _, test := range tests {
